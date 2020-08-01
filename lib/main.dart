8a4130c7f17c322babfce69b0978ea1ff8fce751
korsander <korsander@gmail.com>
2020-08-02 00:30:34 +0300

    added containers for phone and email with icons

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,60 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+7 (000) 000 00 00",
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "stariy-ded@mail.com",
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),

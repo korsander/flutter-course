5dc0c684a0c70670576bebadd243095da6b04ef4
korsander <korsander@gmail.com>
2020-08-01 21:05:04 +0300

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: Text('Hello'),
+            color: Colors.white,
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.only(left: 15.0),
+            padding: EdgeInsets.all(10.0),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
